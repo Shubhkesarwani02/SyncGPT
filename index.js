674aa2279151468b135bcd555a2744093a23d9c4
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-27 02:27:22 +0530

    bot creation done

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith("create")) {
+    const url = message.content.split("create ")[1];
+    return message.reply({
+      content: "Generating  shortId for " + url,
+    });
+  }
+  message.reply({
+    content: "Hi From Bot!!",
+  });
+});
+
+client.on("interactionCreate", (interaction) => {
+  interaction.reply({
+    content: "Pong",
+  });
+});
+
+client.login(
+    process.env.BOT_TOKEN
+);
